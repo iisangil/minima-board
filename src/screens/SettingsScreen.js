e676f3bc2e617a674e0a38d3e3b193f2b1aa254b
Darryl Wong <darrylwong@Darryls-MacBook-Pro.local>
2022-09-29 23:45:05 -0400

    feat: added dark mode toggle

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import { View, Text, Button, StyleSheet, ImageBackground} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {styles} from '../components/Styles.js';
+export const ThemeContext = React.createContext();
 
 export function SettingsScreen({ navigation }) {
+  const { setTheme, theme } = React.useContext(ThemeContext);
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {/*<Button
-          title="Go back to Settings"
-          onPress={() => navigation.push('Settings')}
-        />*/}
-        <Button title="Back to Home" onPress={() => navigation.navigate('Home')} />
-        
-        {/*<Button title="Go back" onPress={() => navigation.goBack()} />*/}
-        {/*<Button
-          title="Go back to first screen in stack"
-          onPress={() => navigation.popToTop()}
-        />*/}
-      </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {/*<Button
+            title="Go back to Settings"
+            onPress={() => navigation.push('Settings')}
+          />*/}
+          <Button title="Switch Theme" onPress={() => setTheme(theme === 'Light' ? 'Dark' : 'Light')} />
+          <Button title="Back to Home" onPress={() => navigation.navigate('Home')} />
+          
+          {/*<Button title="Go back" onPress={() => navigation.goBack()} />*/}
+          {/*<Button
+            title="Go back to first screen in stack"
+            onPress={() => navigation.popToTop()}
+          />*/}
+        </View>
     );
   }
