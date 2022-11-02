c42b9799b5d691a29526097a14c5f4384f5f1512
alexrliu <alexrliu@umich.edu>
2022-11-02 12:56:30 -0400

    implementing gps speed

--- src/components/Speed.js
+++ src/components/Speed.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
+
 import ReactDOM from "react-dom";
 import { View, Text, Button} from 'react-native';
 import { 
   NavigationContainer 
 } from '@react-navigation/native';
+// import Geolocation from '@react-native-community/geolocation';
+import * as Location from 'expo-location';
 
 function SpeedDisplay(){
   // State to store count value
@@ -22,18 +25,44 @@ function SpeedDisplay(){
   };
 
   const color = count < 15 ? 'green' : count > 20 ? 'red' : 'orange';
+
+    // Function to get actual speed
+    const actualSpeed = async () => {
+      // Geolocation.getCurrentPosition(info => console.log(info));
+      // Geolocation.watchPosition(info => console.log(info));
+      // setCount(Geolocation.watchPosition(position.coords.speed))
+      if (await Location.requestForegroundPermissionsAsync()) {
+
+        let watchID = Location.watchPositionAsync(
+          { accuracy: 6, timeInterval: 5000, distanceInterval: 0 },
+          (position) => {
+            // console.log(position);
+            // console.log(position.coords.speed);
+            position.coords.speed < 1 ? setCount(0): setCount(position.coords.speed);
+          }
+        );
+      }
+    };
+
+  useEffect(() => {
+    actualSpeed();
+  }, []);
   
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text style={{ fontSize: '40px', fontWeight: 'bold', color: color, }}> {count} mph </Text>
-    <Button
-      title="Increment Speed"
-      onPress={() => incrementCount()}
-    />
+    {/* <Button
+          title="Increment Speed"
+          onPress={() => incrementCount()}
+        />
     <Button
         title="Decrement Speed"
         onPress={() => decrementCount()}
-    />
+    /> */}
+    {/* <Button
+        title="Find Speed"
+        onPress={() => actualSpeed()}
+    /> */}
     {/* <button onClick={incrementCount}>increment speed</button>
     <button onClick={decrementCount}>decrement speed</button> */}
     </View>
